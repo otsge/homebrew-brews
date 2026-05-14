6c3cf053a963eef5ee227eafeba1b680e84705df
OTSGE <199041525+otsge@users.noreply.github.com>
2026-05-14 13:08:00 +0000

    teldrive: add 1.8.3 bottle.

--- Formula/teldrive.rb
+++ Formula/teldrive.rb
@@ -6,6 +6,16 @@ class Teldrive < Formula
         revision: "d400a2df41db17ba220cd06973fc8df5c6f2854c"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/otsge/brews"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c2de7f3d9ea9deae4501e72cd2f3c9a879029dc434d686ad0b156f805724aabd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1818bc7a23b37c7ac2639e2399bcd27c4c53a950e870f90319028d60eb0e67fb"
+    sha256 cellar: :any_skip_relocation, tahoe:         "34ad941478e32cc163b1ce8228a5947fb383b7e3b2481d208f96a98790b7edc5"
+    sha256 cellar: :any_skip_relocation, sequoia:       "81844c58b1095b79dc54446bc8cdb3ccd26b9c0c5ead577fd0a697e789cb3b86"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3a233fcd0283898b13e0ef41c59d6dde0d48a78a337ca560c0c6685c7933d43b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2c6a42ba485ff9c26f5cd6c40b30c62ac7ce70c4f5282c3344276f192b271db"
+  end
+
   depends_on "go" => :build
   depends_on "go-task" => :build
 
