bd0d52888b9e1b4a31493a972e3e5b9873343f2e
OTSGE <199041525+otsge@users.noreply.github.com>
2026-05-14 13:08:00 +0000

    taproom: add 0.5.0 bottle.

--- Formula/taproom.rb
+++ Formula/taproom.rb
@@ -6,6 +6,16 @@ class Taproom < Formula
   license "MIT"
   head "https://github.com/hzqtc/taproom.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/otsge/brews"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e79efd81018d261aaa6663d93eb028fa51781b16c317c14616b2491936bca0a5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3bdf5aa46dc3521fc3990459e43048b4de0e734abd650f7925a96464a15a29ff"
+    sha256 cellar: :any_skip_relocation, tahoe:         "c17f5192947a0b805aa19f3ea91f215e96b067c6c9f64f4569bc9115ee83f7d9"
+    sha256 cellar: :any_skip_relocation, sequoia:       "0b6965cf3b6c0fda41c4bf55ff4638b8aeb1933bfdf55f2b9ccf13bee3040ce3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2a293eae4311844711248773c8bc795516f7dead7cdf3b9259ee323ec5ac703f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "01efaeba9bf3a94887c02ca63e8386e751a876743cd68cebd7e47947606f70b4"
+  end
+
   depends_on "go" => :build
 
   def install
